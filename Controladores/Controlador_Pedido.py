8310949065fdc828e188121df2c3c11345f5dafd
Jiliard <jiliardmaipeifer172@gmail.com>
2023-10-13 20:55:02 -0300

    alteração nome

---
+++ Controladores/Controlador_Pedido.py
@@ -0,0 +1,29 @@
+from Limites.Tela_Pedido import Tela_Pedido
+from Entidades.Pedido.Pedido import Pedido
+from Controladores import Controlador_Pizzaria
+
+
+class Controlador_Pedido():
+
+    def __init__(self, controlador_pizzaria: Controlador_Pizzaria):
+        self.__lista_pedidos = []
+        self.__Tela_Pedido = Tela_Pedido()
+        self.__Controlador_Pizzaria = controlador_pizzaria
+
+    def fazer_pedido(self, pedido: Pedido):
+        pass
+    
+    def deletar_pedido(self, pedido: Pedido):
+        pass
+    
+    def modificar_pedido(self, pedido: Pedido):
+        pass
+
+    def ver_pedido():
+        pass
+
+    def abre_tela():
+        pass
+
+    def retornar():
+        pass
