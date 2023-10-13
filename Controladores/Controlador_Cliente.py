3094628fa986ba469e228a84798ec739f558731d
Jiliard <jiliardmaipeifer172@gmail.com>
2023-10-13 19:26:07 -0300

    controlador cliente e excecao
    feito controlador cliente e excecao

--- Controladores/Controlador_Cliente.py
+++ Controladores/Controlador_Cliente.py
@@ -1,6 +1,9 @@
+
 from Limites.Tela_Cliente import Tela_Cliente
 from Entidades.Pessoa.Cliente.Cliente import Cliente
 from Controladores import Controlador_Pizzaria
+from excecoes import Cliente_ja_cadastrado
+
 
 class Controlador_CLiente():
 
@@ -9,52 +12,61 @@ class Controlador_CLiente():
         self.__tela_Cliente = Tela_Cliente()
         self.__controlador_pizzaria = controlador_pizzaria
 
-    def cadastrar_cliente(self, cliente: Cliente):
-        for verifica in self.__lista_Clientes:
-            if cliente.cpf == verifica.cpf:
-                return
-
+    def cadastrar_cliente(self):
         dados_cliente = self.__tela_Cliente.pega_dados_cliente()
+        cpf = dados_cliente["cpf"]
+        cliente = self.busca_clientes(cpf)
 
-        novo_cliente = Cliente(dados_cliente["nome"], dados_cliente["cpf"], dados_cliente["telefone"], dados_cliente["endereco"])
+        try:
+            if cliente == None:
+                cliente = Cliente(dados_cliente["nome"], dados_cliente["cpf"], dados_cliente["telefone"], dados_cliente["endereco"])
+                self.__lista_Clientes.append(cliente)
+                self.__tela_Cliente.mostra_mensagem("Cadastro realizado!")
 
-        self.__lista_Clientes.append(novo_cliente)
+            else:
+                raise Cliente_ja_cadastrado(cliente)
+        except Cliente_ja_cadastrado as e:
+            self.__tela_Cliente.mostra_mensagem(e)
 
-    def deletar_cliente(self, cliente: Cliente):
-        #seleciona um cliente pelo cpf
+    def deletar_cliente(self):
+        self.ver_clientes()
         busca_cliente = self.__tela_Cliente.seleciona_cliente()
+        cliente = self.busca_clientes(busca_cliente)
 
-        #busca o cliente atraves do cpf obtido
-        cliente = self.__tela_Cliente.busca_clientes(busca_cliente)
-
-        #remove cliente, se existir
         if cliente is not None:
             self.__lista_Clientes.remove(cliente)
-        #informa um erro, se não existir
+            self.ver_clientes()
+            self.__tela_Cliente.mostra_mensagem("Remoção de cadastro realizado!")
+
         else:
             self.__tela_Cliente.mostra_mensagem("Cliente não cadastrado!")
     
-    def modificar_cliente(self, cliente: Cliente):
-        #seleciona um cliente pelo cpf
+    def modificar_cliente(self):
+        self.ver_clientes()
         busca_cliente = self.__tela_Cliente.seleciona_cliente()
-
-        #busca o cliente atraves do cpf obtido
-        cliente = self.__tela_Cliente.busca_clientes(busca_cliente)
+        cliente = self.busca_clientes(busca_cliente)
 
         if cliente is not None:
-            #recebe os dados do cliente que esta sendo cadastrado
             novos_dados_cliente = self.__tela_Cliente.pega_dados_cliente()
+
             cliente.nome = novos_dados_cliente["nome"]
             cliente.cpf = novos_dados_cliente["cpf"]
             cliente.telefone = novos_dados_cliente["telefone"]
             cliente.endereco = novos_dados_cliente["endereco"]
+
+            self.ver_clientes()
             self.__tela_Cliente.mostra_mensagem("Modificação de cadastro realizado!")
         
         else:
             self.__tela_Cliente.mostra_mensagem("Cliente não cadastrado!")
 
     def ver_clientes(self):
-        pass
+        if self.__lista_Clientes == None:
+            self.__tela_Cliente.mostra_mensagem("Nenhum cliente cadastrado!")
+
+        else:
+            for cliente in self.__lista_Clientes:
+                self.__tela_Cliente.mostra_clientes({"nome": cliente.nome, "cpf": cliente.cpf, "telefone": cliente.telefone, "endereco": cliente.endereco})
 
     def busca_clientes(self, cpf: str):
         #busca cliente solicitado na lista de clientes
---
+++ excecoes.py
@@ -0,0 +1,19 @@
+class Entrada_muito_curta(Exception):
+    def __init__(self):
+        self.mensagem = "Entrada muito curta!"
+        super().__init__(self.mensagem)
+
+class Valor_invalido(Exception):
+    def __init__(self, valores_validos):
+        self.mensagem = "Valor invalido! Os valores validos são: {}"
+        super().__init__(self.mensagem.format(valores_validos))
+
+class Produto_ja_cadastrado(Exception):
+    def __init__(self, produto):
+        self.mensagem = "O produto {} já existe"
+        super().__init__(self.mensagem.format(produto))
+
+class Cliente_ja_cadastrado(Exception):
+    def __init__(self, cliente):
+        self.mensagem = "O cliente {} já está cadastrado"
+        super().__init__(self.mensagem.format(cliente))
