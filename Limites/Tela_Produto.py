fe617802c052c70b4e3207dbce2b5dd93b04ca3d
Jiliard <jiliardmaipeifer172@gmail.com>
2023-11-26 20:57:16 -0300

    arrumado

--- Limites/Tela_Produto.py
+++ Limites/Tela_Produto.py
@@ -15,7 +15,7 @@ class Tela_Produto:
         button, values = self.__window.Read()
         return button, values
 
-    def init_components(self):
+    def init_components(self, lista_pizzas):
 
         sg.ChangeLookAndFeel('DarkBrown1')
         font = ("Palatino Linotype", 10)
@@ -41,9 +41,9 @@ class Tela_Produto:
         self.__window = sg.Window('Pizzaria', default_element_size=(40, 1), size=(1250,620),
                                   icon="Imagens\pizza icone.ico").Layout(layout)
 
-    def abre_tela(self):
+    def abre_tela(self, lista_pizzas):
 
-        self.init_components()
+        self.init_components(lista_pizzas)
         button, values = self.__window.Read()
         button = int(button)
         opcao = 0
